0a626f4be963d6b0b101988ef812213b64a286d8
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-11-23 10:37:26 +0900

    DI service to controller

---
+++ src/main/kotlin/com/kotlinspring/coursecatalogservice/service/GreetingService.kt
@@ -0,0 +1,9 @@
+package com.kotlinspring.coursecatalogservice.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class GreetingService {
+
+    fun retrieveGreeting(name: String) = "Hello $name"
+}
